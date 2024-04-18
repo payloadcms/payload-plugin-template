94c59e3fb76a7e9b5e8c11fb8e12f07d72bf6da6
Paul Popus <paul@nouance.io>
2024-04-18 15:47:28 -0300

    chore: update directory to use the same node_modules across the project

--- test/plugin.spec.ts
+++ test/plugin.spec.ts
@@ -1,13 +1,18 @@
 import type { Server } from 'http'
 import mongoose from 'mongoose'
 import payload from 'payload'
-import { start } from './src/server'
+//import { start } from './src/server'
+import { nextDev } from 'next/dist/cli/next-dev.js'
 
 describe('Plugin tests', () => {
   let server: Server
 
+  console.log('run start')
+
   beforeAll(async () => {
-    await start({ local: true })
+    console.log('run start')
+    //await start({ local: true })
+    nextDev({ port: 3000 }, 'default', '../dev')
   })
 
   afterAll(async () => {
