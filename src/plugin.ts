07d3cf3bf6d16188e08a0a72db93013e0ccfa184
Elliot DeNolf <denolfe@gmail.com>
2024-10-07 10:37:02 -0400

    chore: add components

--- src/plugin.ts
+++ src/plugin.ts
@@ -2,47 +2,85 @@ import type { Config } from 'payload'
 
 import type { MyPluginOptions } from './types.js'
 
+import { onInitExtension } from './lib/onInitExtension.js'
 
 export const myPlugin =
   (pluginOptions: MyPluginOptions) =>
   (incomingConfig: Config): Config => {
-    let config = { ...incomingConfig }
+    const config = { ...incomingConfig }
 
-    if (pluginOptions.debug) {
-      // eslint-disable-next-line no-console
-      console.log('Entering myPlugin')
+    config.admin = {
+      ...(config.admin || {}),
+
+      // Add additional admin config here
+
+      components: {
+        ...(config.admin?.components || {}),
+        // Add additional admin components here
+        afterDashboard: [
+          '/components/AfterDashboard/index.js#AfterDashboard',
+          '/components/AfterDashboardClient/index.js#AfterDashboardClient',
+        ],
+      },
     }
 
     /**
-     * Make changes to the config object here
+     * If the plugin is disabled, return the config without modifying it
      *
-     * Typically, you would:
-     * - loop through the collections and fields
-     * - spread any existing fields and collections into new objects
-     * - make changes to the the specific collection/field
+     * Be cautious when using this if your plugin adds new collections or fields
+     * as this could cause issues w/ Postgres migrations
      */
-
-    config = {
-      ...config,
-      collections: (config.collections || []).map((collection) => {
-        const modifiedCollection = { ...collection }
-
-        /**
-         * Make changes to the collection here
-         */
-
-        modifiedCollection.fields = (modifiedCollection.fields || []).map((field) => {
-          const newField = { ...field }
-
-          /**
-           * Make changes to the fields here
-           */
-
-          return newField
-        })
-
-        return modifiedCollection
-      }),
+    if (pluginOptions.enabled === false) {
+      return config
     }
+
+    config.collections = (config.collections || []).map((collection) => {
+      const modifiedCollection = { ...collection }
+
+      // Make changes to the collection here
+
+      modifiedCollection.fields = (modifiedCollection.fields || []).map((field) => {
+        const newField = { ...field }
+
+        // Make changes to the fields here
+
+        return newField
+      })
+
+      return modifiedCollection
+    })
+
+    // Add additional collections here
+
+    config.endpoints = [
+      ...(config.endpoints || []),
+      {
+        handler: () => {
+          return Response.json({ message: 'Hello, world!' })
+        },
+        method: 'get',
+        path: '/custom-endpoint',
+      },
+      // Add additional endpoints here
+    ]
+
+    config.globals = [
+      ...(config.globals || []),
+      // Add additional globals here
+    ]
+
+    config.hooks = {
+      ...(config.hooks || {}),
+      // Add additional hooks here
+    }
+
+    config.onInit = async (payload) => {
+      if (incomingConfig.onInit) {
+        await incomingConfig.onInit(payload)
+      }
+      // Add additional onInit code by using the onInitExtension function
+      onInitExtension(pluginOptions, payload)
+    }
+
     return config
   }
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
 export type MyPluginOptions = {
   debug?: boolean
+  enabled?: boolean
 }
