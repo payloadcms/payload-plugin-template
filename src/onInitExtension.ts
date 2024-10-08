2960a49f4d436379280140dce377ca1d332005dd
Elliot DeNolf <denolfe@gmail.com>
2024-10-08 10:36:03 -0400

    Revert "chore: migrate template to use latest beta of payload packages"
    This reverts commit b8b6d173891100c5b98a71e20e1b167871e1f47c.

--- src/onInitExtension.ts
+++ src/onInitExtension.ts
@@ -1,8 +1,12 @@
-import type { Payload } from 'payload'
+import type { Payload } from 'payload/dist/payload'
 
 import type { PluginTypes } from './types'
 
 export const onInitExtension = (pluginOptions: PluginTypes, payload: Payload): void => {
+  const { express: app } = payload
+
+  if (!app) return
+
   try {
     // You can use the existing express app here to add middleware, routes, etc.
     // app.use(...)
--- src/plugin.ts
+++ src/plugin.ts
@@ -2,6 +2,7 @@ import type { Config, Plugin } from 'payload/config'
 
 import { onInitExtension } from './onInitExtension'
 import type { PluginTypes } from './types'
+import { extendWebpackConfig } from './webpack'
 import AfterDashboard from './components/AfterDashboard'
 import newCollection from './newCollection'
 
@@ -9,62 +10,70 @@ type PluginType = (pluginOptions: PluginTypes) => Plugin
 
 export const samplePlugin =
   (pluginOptions: PluginTypes): Plugin =>
-  incomingConfig => {
-    let config = { ...incomingConfig }
+    (incomingConfig) => {
+      let config = { ...incomingConfig }
 
-    config.admin = {
-      ...(config.admin || {}),
+      // If you need to add a webpack alias, use this function to extend the webpack config
+      const webpack = extendWebpackConfig(incomingConfig)
 
-      // Add additional admin config here
+      config.admin = {
+        ...(config.admin || {}),
+        // If you extended the webpack config, add it back in here
+        // If you did not extend the webpack config, you can remove this line
+        webpack,
 
-      components: {
-        ...(config.admin?.components || {}),
-        // Add additional admin components here
-        afterDashboard: [...(config.admin?.components?.afterDashboard || []), AfterDashboard],
-      },
-    }
+        // Add additional admin config here
 
-    // If the plugin is disabled, return the config without modifying it
-    // The order of this check is important, we still want any webpack extensions to be applied even if the plugin is disabled
-    if (pluginOptions.enabled === false) {
+        components: {
+          ...(config.admin?.components || {}),
+          // Add additional admin components here
+          afterDashboard: [
+            ...(config.admin?.components?.afterDashboard || []),
+            AfterDashboard,
+          ],
+        },
+      }
+
+      // If the plugin is disabled, return the config without modifying it
+      // The order of this check is important, we still want any webpack extensions to be applied even if the plugin is disabled
+      if (pluginOptions.enabled === false) {
+        return config
+      }
+
+      config.collections = [
+        ...(config.collections || []),
+        // Add additional collections here
+        newCollection, // delete this line to remove the example collection
+      ]
+
+      config.endpoints = [
+        ...(config.endpoints || []),
+        {
+          path: '/custom-endpoint',
+          method: 'get',
+          root: true,
+          handler: (req, res): void => {
+            res.json({ message: 'Here is a custom endpoint' });
+          },
+        },
+        // Add additional endpoints here
+      ]
+
+      config.globals = [
+        ...(config.globals || []),
+        // Add additional globals here
+      ]
+
+      config.hooks = {
+        ...(config.hooks || {}),
+        // Add additional hooks here
+      }
+
+      config.onInit = async payload => {
+        if (incomingConfig.onInit) await incomingConfig.onInit(payload)
+        // Add additional onInit code by using the onInitExtension function
+        onInitExtension(pluginOptions, payload)
+      }
+
       return config
     }
-
-    config.collections = [
-      ...(config.collections || []),
-      // Add additional collections here
-      newCollection, // delete this line to remove the example collection
-    ]
-
-    config.endpoints = [
-      ...(config.endpoints || []),
-      {
-        path: '/custom-endpoint',
-        method: 'get',
-        handler: async req => {
-          return new Response(JSON.stringify({ message: 'Here is a custom endpoint' }), {
-            status: 200,
-          })
-        },
-      },
-      // Add additional endpoints here
-    ]
-
-    config.globals = [
-      ...(config.globals || []),
-      // Add additional globals here
-    ]
-
-    config.hooks = {
-      ...(config.hooks || {}),
-      // Add additional hooks here
-    }
-
-    config.onInit = async payload => {
-      if (incomingConfig.onInit) await incomingConfig.onInit(payload)
-      // Add additional onInit code by using the onInitExtension function
-      onInitExtension(pluginOptions, payload)
-    }
-
-    return config
-  }
